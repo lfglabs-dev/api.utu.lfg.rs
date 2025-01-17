3f05fcdbe2ebfd0d17b32d2c78b30f9b3a3c0faf
Iris <iris.devillars@gmail.com>
2025-01-17 09:25:36 +0100

    fix: feedbacks from review

--- src/server/bitcoin_deposits.rs
+++ src/server/bitcoin_deposits.rs
@@ -11,14 +11,8 @@ use axum::Json;
 use axum_auto_routes::route;
 use mongodb::bson::doc;
 use reqwest::StatusCode;
-use serde::{Deserialize, Serialize};
 
 use super::responses::{ApiResponse, Status};
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct BitcoinDepositsQuery {
-    starknet_receiving_addresses: Vec<String>,
-}
 
 #[route(get, "/bitcoin_deposits")]
 pub async fn bitcoin_deposits<B>(
@@ -26,15 +20,6 @@ pub async fn bitcoin_deposits<B>(
     request: Request<B>,
 ) -> impl IntoResponse {
     let mut session = try_start_session!(state);
-    if let Err(err) = session.start_transaction().await {
-        return (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            Json(ApiResponse::new(
-                Status::InternalServerError,
-                format!("Database error: {:?}", err),
-            )),
-        );
-    };
 
     let query_string = request.uri().query().unwrap_or_default();
     let params: HashMap<String, Vec<String>> = url::form_urlencoded::parse(query_string.as_bytes())
