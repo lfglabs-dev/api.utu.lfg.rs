d0faef2e87e450b91d828b8c6008ae7dddf89569
Iris <iris.devillars@gmail.com>
2025-02-07 14:56:02 +0100

    fix: remove claim function

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -2,7 +2,6 @@ pub mod auth;
 pub mod bitcoin_deposits;
 pub mod bitcoin_to_starknet_mapping;
 pub mod bitcoin_withdrawals;
-pub mod claim_deposit_data;
 pub mod deposit_claim_txhash;
 pub mod get_bitcoin_deposit_addr;
 pub mod get_deposits;
