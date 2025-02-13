47bbbfddf472de8b548ec090ce055c5403559aca
Iris <iris.devillars@gmail.com>
2025-02-13 16:10:43 +0100

    fix: remove unused code

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -6,9 +6,6 @@ use crate::pub_struct;
 
 pub mod deposit_activity;
 pub mod macros;
-pub mod msg_hash;
-pub mod runes;
-pub mod starknet;
 pub mod withdrawals;
 
 pub_struct!(Debug, Clone, Copy, PartialEq, Eq; Address {
